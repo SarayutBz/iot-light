7ee5a06295008e1920949666c8a29433d73ef252
Wattanapongphan <wattanapongphan@gmail.com>
2025-03-06 13:35:12 +0700

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ControlLight from '../views/contolLight.vue'
+import Login from '../views/login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,11 @@ const router = createRouter({
       name: 'controlLight',
       component: ControlLight,
     },
-    
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
   ],
 })
 
